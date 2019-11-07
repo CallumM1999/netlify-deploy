fd0c377becc5fcd9d1176205c3cd0e3419744208
Callum Macpherson <callummac@protonmail.com>
2019-11-07 15:41:40 +0000

    styled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,21 @@
 import React from "react"
+import styled, { createGlobalStyle } from 'styled-components'
 
-export default () => <div>Hello world! fclgv</div>
+const Nav = styled.nav`
+    background-color: red;
+`;
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        margin: 0;
+        padding: 0;
+    }
+`;
+
+export default () => (
+    <>
+        <GlobalStyle/>
+        <Nav>nav</Nav>
+        <div>Hello world! fclgv</div>
+    </>
+)
